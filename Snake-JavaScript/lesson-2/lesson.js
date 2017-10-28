3c3558120589c9cb093807af2175ac3f06de357a
bladechapman <bechapm2@illinois.edu>
2017-10-28 11:57:11 -0700

    lesson-2

--- Snake-JavaScript/lesson-2/lesson.js
+++ Snake-JavaScript/lesson-2/lesson.js
@@ -1,9 +1,22 @@
 createBoard();
 
 let mySnake = new Snake(24, 24);
+let myApple = new Apple(25,25);
+myApple.render()
 
-document.addEventListener('keyup', (event) => {
+document.addEventListener('keyup', function (event) {
     const keyName = event.key;
+
+    if(mySnake.direction==Snake.SOUTH && keyName == "ArrowUp"){
+      gameOver()
+    }else if(mySnake.direction==Snake.NORTH && keyName=="ArrowDown"){
+      gameOver()
+    }else if(mySnake.direction==Snake.EAST && keyName=="ArrowLeft"){
+      gameOver()
+    }else if(mySnake.direction==Snake.WEST && keyName=="ArrowRight"){
+      gameOver()
+    }
+
     if (keyName === "ArrowUp") {
         mySnake.direction = Snake.NORTH;
     }
@@ -16,12 +29,57 @@ document.addEventListener('keyup', (event) => {
     else if (keyName === "ArrowLeft") {
         mySnake.direction = Snake.WEST;
     }
+
 });
 
 function renderCycle() {
     clearBoard();
     mySnake.move();
     mySnake.render();
+    myApple.render();
+    growSnake();
+
 }
 
-let endGame = startGame(renderCycle, 1000);
+function growSnake(){
+  if(checkCoord()==true){
+    mySnake.grow()
+     moveApple()
+  }else{
+
+  }
+}
+function checkCoord(){
+  if(mySnake.headX== myApple.x && mySnake.headY==myApple.y){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+// parseInt(Math.random() * 50)
+function moveApple(){
+  myApple.x=parseInt(Math.random()*50)
+  myApple.y=parseInt(Math.random()*50)
+}
+
+// mySnake.grow()
+
+let endGame = startGame(renderCycle, 500);
+
+// function ageCalculator2(day, month, year){
+//   if(month>12 || month<1 || day>31 || day<1){
+//     return console.error('Invalid numbers');
+//   }
+//
+//   if(month>10){
+//     return year-1995
+//   }else if(month==10 && day>=4){
+//     return year-1995
+//   }else if(month==10 && day<4){
+//     return year-1996
+//   }else{
+//     return year - 1996
+//   }
+// }
