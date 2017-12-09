4e7351377c49f0f8d93b8c0470479776b9c42eea
bladechapman <bladechapman@gmail.com>
2017-12-09 12:13:24 -0800

    lesson 2 complete

--- HangMan/lesson-2/exercises.js
+++ HangMan/lesson-2/exercises.js
@@ -1,19 +1,30 @@
 
 // 1. make a function called isPalindrome that tests to see if a word is a palindrome
 function isPalindrome(word) {
-    return undefined;
+  let wordArray = word.split('')
+  let originalWordArray = word.split('')
+  let reverseWordArray = wordArray.reverse()
+  for(let i=0; i<wordArray.length; i++){
+    if(originalWordArray[i]!=reverseWordArray[i]){
+      return false;
+    }
+  }
+    return true;
 }
 
-// 2. make a number called accumulateFives that only adds a number to an array if 
+// 2. make a number called accumulateFives that only adds a number to an array if
 // the number is greater than 5
 function accumulateFives(array, number) {
     return [];
 }
 
-// 3. make a function called mapString that turns all the items in an array to 
+// 3. make a function called mapString that turns all the items in an array to
 // strings using obj.toString
 function mapString(array) {
-    return [];
+let newArray = array.map(function (currentValue){
+  return currentValue.toString();
+ })
+    return newArray;
 }
 
 // 4. make a function called sumTwos that sums all the multiples of twos in an array
--- HangMan/lesson-2/lesson.js
+++ HangMan/lesson-2/lesson.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const readline = require('readline')
 
 // look up the readline library, and see if you can figure out how to read input from the command line
 
@@ -11,8 +12,41 @@ function getWord() {
 }
 
 let chosenWord = getWord();
+let chosenWordArray = chosenWord.split('')
 let usedLetters = [];
 let exposedLetters = new Array(chosenWord.length);
 for (let i = 0; i < exposedLetters.length; i++) {
   exposedLetters[i] = false;
 }
+
+function checkLetter(letter, array, revealedLetters){
+  for(let i=0; i<array.length; i++){
+    if(letter == array[i]){
+      revealedLetters[i]=true
+    }
+  }
+}
+
+function printState(exposed, word){
+let wordString = ""
+  for(let i=0; i<exposed.length; i++){
+    if(exposed[i]==true){
+      wordString =  wordString + word[i]
+    }else{
+      wordString = wordString + '_'
+    }
+  }
+  console.log(wordString)
+}
+const rl =readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+console.log(chosenWord)
+rl.question('Choose a letter', function (response){
+console.log(response)
+usedLetters.push(response)
+checkLetter(response, chosenWordArray, exposedLetters)
+printState(exposedLetters, chosenWordArray)
+rl.close()
+ })
