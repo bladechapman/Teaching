ebae9638d7237f2d2dcb58f2ecdcb202a0c6213a
blade-chapman <blade_chapman@apple.com>
2018-04-21 14:42:14 -0700

    cleanup

--- Systems/lesson-3/consh/main.c
+++ Systems/lesson-3/consh/main.c
@@ -10,41 +10,47 @@ char* retrieveInput() {
 
     return buffer;
 }
-char* rangeCopy(char* input, int j, int i){
-	char* dest = malloc(i-j);
-	char* newStart = &input[j];
 
-	strncpy(dest, newStart, i-j);
-	return dest;
+char* rangeCopy(char* input, int j, int i) {
+    char* dest = malloc(i - j);
+    char* newStart = &input[j];
+
+    strncpy(dest, newStart, i-j);
+    return dest;
 }
-char** separateStrings(char* input){
-	int j = 0;
-	int i = 0;
-	int spaceCount =0;
-	int arrayLocation = 0;
-	for(int k=0; k<strlen(input); k++){
-		if(input[k]==' '){
-			spaceCount = spaceCount+1;
-		}
-	}
-	char** array = malloc((spaceCount+1) * sizeof(char*));
-	for(; i<strlen(input); i++){
-		if(input[i]==' '){
-			array[arrayLocation] = rangeCopy(input, j, i);
-			arrayLocation += 1;
-			j=i+1;
-		}
-	}
-	array[arrayLocation] = rangeCopy(input, j, i);
-	return array;
+
+int countChars(char* input, char target) {
+    const int length = strlen(input);
+    int count = 0;
+    for (int i = 0; i < length; i++) {
+        if (target == input[i]) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+char** separateStrings(char* input) {
+    int j = 0;
+    int i = 0;
+
+    const int spaceCount = countChars(input, ' ');
+
+    int arrayLocation = 0;
+    char** array = malloc((spaceCount + 1) * sizeof(char*));
+    for(; i < strlen(input); i++) {
+        if(input[i] == ' ') {
+            array[arrayLocation] = rangeCopy(input, j, i);
+            arrayLocation += 1;
+            j = i + 1;
+        }
+    }
+    array[arrayLocation] = rangeCopy(input, j, i);
+    return array;
 }
 
 int main(int argc, char** argv) {
-
-   /* char* str = retrieveInput();
-    printf("%s", str);
-
-    return 0;*/
-   // printf("%s", rangeCopy( "ksdjf", 1, 3));
-	printf("%s\n", separateStrings("hello world hi")[2]);
+    printf("%s\n", separateStrings("hello world hi")[2]);
+    return 0;
 }
