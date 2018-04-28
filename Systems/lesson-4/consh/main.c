08623dcfe11c7732d3485910f4db3a4ef25762e1
blade-chapman <blade_chapman@apple.com>
2018-04-28 12:44:07 -0700

    lesson-4 complete

--- Systems/lesson-4/consh/main.c
+++ Systems/lesson-4/consh/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <dirent.h>
 #include "lib/splitString.h"
 
 char* retrieveInput() {
@@ -13,10 +13,78 @@ char* retrieveInput() {
     return buffer;
 }
 
+char* ls(){
+    DIR* currentDirectory = opendir(".");
+    struct dirent *returnValue = readdir(currentDirectory);
+    char* directoryName = (*returnValue).d_name;
 
-int main(int argc, char** argv) {
-    printf("%s\n", splitString("hello world hi", ' ')[2]);
-
-    return 0;
+    return directoryName;
 }
 
+int main(int argc, char** argv) {
+    // printf("%s\n", splitString("hello world hi", ' ')[2]);
+
+/*    char** myStrings = splitString("hello world hi", ' ');
+
+    for(int i=0; i<3; i++){
+        free(myStrings[i]);
+    }
+    free(myStrings);
+
+
+    return 0;*/
+
+    // printf("%d", strcmp("end", "end"));
+
+    // while (0 < 1) {
+    //     char* input = retrieveInput();
+    //     int numWords = countChars(input, ' ')+1;
+    //     char** output = splitString(input,' ');
+    //
+    //     for(int i=0; i<numWords; i++){
+    //         printf("%s\n", output[i]);
+    //
+    //
+    //         if(strncmp(output[i], "ls", 2)){
+    //             printf("%s\n","ls detected");
+    //         }
+    //     }
+    // }
+    printf("%s\n", ls());
+    //
+    // typedef int myInt;
+    // myInt crazyStuff = 0;
+    //
+    //
+    // struct myIntArray {
+    //     int *data;
+    //     int length;
+    // }
+    // typedef struct _myIntArray myIntArray;
+
+
+    // typedef struct _intArray {
+    //     int *data;
+    //     int length;
+    // } intArray;
+    //
+    // // intArray testValue = malloc(sizeof(intArray));
+    // intArray testValue;
+    // testValue.data = malloc(sizeof(int)*4);
+    // testValue.data[0] = 1;
+    // testValue.length = 13;
+
+
+    // printf("%d\n", testValue.length);
+
+
+    // struct myStruct {
+    //     int firstNumber;
+    //     int SecondNumber;
+    // }
+    //
+    //
+    // struct myStruct myValue;
+    // myValue.firstNumber = 0;
+    // myValue.secondNumber = 1;
+}
