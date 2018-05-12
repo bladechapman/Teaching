5779941d616c149375b6ada810e694b6d921b031
Connor <jianmingyu@jianmingtekimbp.attlocal.net>
2018-05-12 12:30:42 -0700

    lesson-6 complete

--- Systems/lesson-5/consh/main.c
+++ Systems/lesson-5/consh/main.c
@@ -14,20 +14,61 @@ char* retrieveInput() {
     return buffer;
 }
 
-char* ls(){
-    DIR* currentDirectory = opendir(".");
-    struct dirent *returnValue = readdir(currentDirectory);
-    char* directoryName = (*returnValue).d_name;
+int directoryLength() {
+	// 1. look at the current directory
+	// 2. declare an integer variable
+	// 3. while loop
+	// if readdir != NULL, x = x + 1
+	// if readdir == NULL, break
+	// return x
+	DIR* currentDirectory = opendir(".");
+	int x = 0;
 
-    return directoryName;
+	while(1){
+		struct dirent *currentEntry = readdir(currentDirectory);
+		if(currentEntry!=NULL){
+			x+=1;
+		}else{
+			break;
+		}
+	}
+	return x;
+}
+
+char** ls(){
+    DIR* currentDirectory = opendir(".");
+   	char** directoryNames = malloc(sizeof(char*)*directoryLength());
+    int x =0;
+    while(1){
+ 		struct dirent *currentEntry = readdir(currentDirectory);
+ 		if(currentEntry==NULL){
+ 			break;
+ 		}else{
+ 			char* entryName = currentEntry -> d_name;
+ 			directoryNames[x] = entryName;
+ 			x+=1;
+ 		}
+
+ 	}
+ 		
+   return directoryNames;
 }
 
 int main(int argc, char** argv) {
-    char** pieces = splitString("hello world hi", ' ');
-    printf("%d\n", strcmp("hello", pieces[0]));
+   // char** pieces = splitString("hello world hi", ' ');
+    //printf("%d\n", strcmp("hello", pieces[0]));
 
-    char *output = ls();
-    printf("%s\n", output);
+   // char *output = ls();
+   // printf("%s\n", output);
 
-    return 0;
+
+	char** names = ls();
+   for(int i =0; i<directoryLength(); i++){
+   	printf("%s\t", names[i]);
+   }
+   printf("\n");
+
+
+
+    //return 0;
 }
