3df718ea31b2e7d4fa1f30baafe1bcb9e5a95cea
blade-chapman <blade_chapman@apple.com>
2018-05-26 10:18:28 -0700

    whoops

--- Systems/lesson-7/notes/main.c
+++ Systems/lesson-7/notes/main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <dirent.h>
 #include <signal.h>
-
+#include <unistd.h>
+#include <errno.h>
 #include "lib/splitString.h"
 #include "lib/conshLs.h"
 #include "lib/log.h"
@@ -25,13 +26,39 @@ int main(int argc, char** argv) {
         input[strlen(input) - 1] = '\0';
         char **splits = splitString(input, ' ');
 
-        if (strcmp(splits[0], "ls") == 0) {
-            consh_folder *folder = ls();
-            consh_folder_print(folder);
+        pid_t child_pid = fork();
+        if (child_pid == -1) {
+            // Something went wrong with forking
+            printf("Something went wrong!\n");
+            exit(1);
         }
-        else if (strcmp(splits[0], "!") == 0) {
-            printLog(logInstance);
+        else if (child_pid == 0) {
+            const int num_words = countChars(input, ' ') + 1;
+
+            if (num_words > 0) {
+                char *new_args[num_words + 1];
+
+                for (int i = 0; i < num_words; i++) {
+                    new_args[i] = splits[i];
+                }
+                new_args[num_words] = NULL;
+
+                execvp(new_args[0], new_args);
+                printf("Something went wrong with exec: %d\n", errno);
+                exit(1);
+            }
+            else {
+                printf("Please enter a command\n");
+                exit(0);
+            }
         }
+        else {
+            // We're in the parent
+            int status;
+            waitpid(child_pid, &status, 0);
+            printf("\e[33mprocess %d completed with status %d\n\e[0m", child_pid, status);
+        }
+
 
         addToLog(input, logInstance);
     }
